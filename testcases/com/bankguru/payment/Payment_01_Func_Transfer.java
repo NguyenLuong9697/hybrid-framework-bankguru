82a9a2d42e1cb43ff4cc1258398ffe7a8a016c09
Nguyen Thi Luong <nguyenluong09061997@gmail.com>
2021-07-31 10:36:11 +0700

    Init new automation test framework

---
+++ testcases/com/bankguru/payment/Payment_01_Func_Transfer.java
@@ -0,0 +1,20 @@
+package com.bankguru.payment;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Payment_01_Func_Transfer {
+  
+  @BeforeClass
+  public void beforeClass() {
+  }
+  @Test
+  public void TC_01() {
+  }
+ 
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
---
+++ testdata/com/bankguru/data/Payment.java
@@ -0,0 +1,13 @@
+package com.bankguru.data;
+
+public class Payment {
+	public class FuncTransfer{
+		
+	}
+	public class Deposit{
+			
+	}
+	public class Withdrawal{
+		
+	}
+}
